63e62a6a094caa122f8b87e3e1ec229ea56137c1
Andrew Oslica <asoslica@gmail.com>
2019-08-10 21:39:18 -0400

    Added create budget, experimented with pie charts

--- client/src/components/Expenses.component.tsx
+++ client/src/components/Expenses.component.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import { Paper, Select } from '@material-ui/core';
-import { IUserState, IState } from '../redux';
 import { connect } from 'react-redux';
-import { Input, Label, Container, Row, Popover } from 'reactstrap';
+import { Container } from 'reactstrap';
+import { IState, IUserState } from '../redux';
 import NewExpense from './NewExpenseDialog';
 
 interface IExpenseProps {
   user: IUserState;
-  type:number;
-  date:string;
-  description:string;
-  amount:number;
+  type: number;
+  date: string;
+  description: string;
+  amount: number;
 }
 
-function Expenses(props:IExpenseProps ) {
+function Expenses(props: IExpenseProps) {
   return (
     <Container style={{ textAlign: 'center' }}>
-      <h2>Manage your expenses, {props.user.first}</h2>  
+      <h2>Manage your expenses, {props.user.first}</h2>
       {/* Here is the create new expense form. 
           The axios request is sent thru there. */}
       {/* Send the user Id to let the database know
           who made the expense. */}
       {NewExpense(props.user.id)}
-      <br/> 
+      <br />
     </Container>
   );
 }
